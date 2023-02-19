a3eaacfd02f750db1a35af01a3c0530b2c46b000
Arben Apura <arbenapura.official@gmail.com>
2023-02-20 01:07:07 +0800

    Fixed bugs

--- src/components/layouts/Header.svelte
+++ src/components/layouts/Header.svelte
@@ -1,7 +1,12 @@
+<script lang="ts">
+	// IMPORTED ASSETS
+	import IconSVG from '$assets/icon.svg';
+</script>
+
 <header class="h-[60px] flex items-center">
 	<a class="flex items-center" href="/">
 		<div class="w-[60px] p-2">
-			<img src="icon.svg" alt="" />
+			<img src={IconSVG} alt="" />
 		</div>
 		<h6 class="font-bold">CPU Scheduler</h6>
 	</a>
